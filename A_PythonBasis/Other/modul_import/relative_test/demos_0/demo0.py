de1cef88bd42cb3684123acbc02d9b1994860c12
dcdmm <dcdmmcomeon@gmail.com>
2022-02-28 13:17:45 +0800

    相对路径导入的完善

---
+++ A_PythonBasis/Other/modul_import/relative_test/demos_0/demo0.py
@@ -0,0 +1,8 @@
+print('__file__={0:<35} | __name__={1:<20} | __package__={2:<20}'.format(__file__, __name__, str(__package__)))
+from .. import config  # ..表示父级目录(.表示当前目录)
+
+print("The value of config.count is {0}".format(config.count))
+
+
+def hello_demo0():
+    print(config.count)
---
+++ A_PythonBasis/Other/modul_import/relative_test_main.py
@@ -0,0 +1,12 @@
+print('__file__={0:<35} | __name__={1:<20} | __package__={2:<20}'.format(__file__, __name__, str(__package__)))
+# __file__:当前*.py文件的路径
+# __package__:When it is present, relative imports will be based on this attribute rather than the module __name__ attribute;当前文件__package__属性为None
+
+from relative_test.demos_0.demo0 import hello_demo0
+from relative_test.demos_0.demos_1.demo1 import hello_demo1
+
+# 项目relative_test中包含相对路径(../...)导入的代码
+# 只能在与项目relative_test目录结构平级或更高的python文件中运行
+hello_demo0()
+print('****************************************')
+hello_demo1()

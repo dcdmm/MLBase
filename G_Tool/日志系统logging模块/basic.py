7e1ce201203307926c1511d0790b917fc0a855b4
dcdmm <dcdmmcomeon@gmail.com>
2022-04-13 21:32:20 +0800

    logging模块有了基本的认识

---
+++ G_Tool/日志系统logging模块/basic.py
@@ -0,0 +1,27 @@
+import logging
+
+'''
+Does basic configuration for the logging system by creating a StreamHandler with a default Formatter and adding it to the root logger. 
+The functions debug(), info(), warning(), error() and critical() will call basicConfig() automatically if no handlers are defined for the root logger.
+
+This function does nothing if the root logger already has handlers configured, unless the keyword argument force is set to True.
+'''
+logging.basicConfig(
+    # Specifies that a FileHandler be created, using the specified filename, rather than a StreamHandler.
+    filename='basic.log',  # 日志保存位置(默认输出到控制台)
+    # If filename is specified, open the file in this mode. Defaults to 'a'.
+    filemode='a',  # 日志文件的打开模式
+    # Set the root logger level to the specified level.
+    level=logging.DEBUG,  # 日志级别
+    # se the specified format string for the handler.
+    # Defaults to attributes levelname, name and message separated by colons.
+    format="%(asctime)s %(name)s %(levelname)s %(message)s",
+    # Use the specified date/time format, as accepted by time.strftime().
+    datefmt='%Y-%m-%d  %H:%M:%S %a'
+)
+
+logging.debug(msg="msg1")
+logging.info(msg="msg2")
+logging.warning(msg="msg3")
+logging.error(msg="msg4")
+logging.critical(msg="msg5")
---
+++ G_Tool/日志系统logging模块/example.py
@@ -0,0 +1,23 @@
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(lineno)d - %(message)s ',
+                    filename='example.log')
+
+
+def division(a, b):
+    return (a + 1) / b
+
+
+number1_list = [34, 20., 34]
+number2_list = [80, 3., 0.]
+
+for i in range(len(number1_list)):
+    try:
+        result = number1_list[i] / number2_list[i]
+        if (type(number1_list[i]) or type(number2_list[i])) != float:
+            logging.warning(msg="no float type")
+        else:
+            logging.info(msg="result=" + str(result))
+    except ZeroDivisionError as reason:
+        logging.error("Error" + str(reason))

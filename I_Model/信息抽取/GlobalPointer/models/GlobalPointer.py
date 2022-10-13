51b697e766588891b95ca4a1ad120485b6ea107f
dcdmm <dcdmmcomeon@gmail.com>
2022-10-13 16:20:36 +0800

    信息抽取第一阶段理解达到

---
+++ I_Model/信息抽取/GlobalPointer/models/GlobalPointer.py
@@ -0,0 +1,88 @@
+import torch
+import numpy as np
+import torch.nn as nn
+
+
+class GlobalPointer(nn.Module):
+    def __init__(self, encoder, ent_type_size, inner_dim, RoPE=True):
+        super().__init__()
+        self.encoder = encoder
+        self.ent_type_size = ent_type_size
+        self.inner_dim = inner_dim
+        self.hidden_size = encoder.config.hidden_size
+        self.dense = nn.Linear(self.hidden_size, self.ent_type_size * self.inner_dim * 2)
+        self.RoPE = RoPE
+
+    def sinusoidal_position_embedding(self, batch_size, seq_len, output_dim):
+        """旋转式位置编码RoPE"""
+        # position_ids.shape=[seq_len, 1]
+        position_ids = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(-1)
+        # indices.shape=[output_dim // 2]
+        indices = torch.arange(0, output_dim // 2, dtype=torch.float)
+        # indices.shape=[output_dim // 2]
+        indices = torch.pow(10000, -2 * indices / output_dim)
+        # embeddings.shape=[seq_len, output_dim // 2]
+        embeddings = position_ids * indices
+        # embeddings.shape=[seq_len, output_dim // 2, 2]
+        embeddings = torch.stack([torch.sin(embeddings), torch.cos(embeddings)], dim=-1)
+        # embeddings.shape=[seq_len * batch_size, output_dim // 2, 2]
+        embeddings = embeddings.repeat((batch_size, *([1] * len(embeddings.shape))))
+        # embeddings.shape=[batch_size, seq_len, output_dim]
+        embeddings = torch.reshape(embeddings, (batch_size, seq_len, output_dim))
+        embeddings = embeddings.to(self.device)
+        return embeddings
+
+    def forward(self, input_ids, attention_mask, token_type_ids):
+        # input_ids.shape=[batch_size, seq_len]
+        batch_size = input_ids.size()[0]
+        seq_len = input_ids.size()[1]
+        self.device = input_ids.device
+
+        context_outputs = self.encoder(input_ids, attention_mask, token_type_ids)
+        # last_hidden_state.shape=[batch_size, seq_len, hidden_size]
+        last_hidden_state = context_outputs[0]
+
+        # outputs.shape=[batch_size, seq_len, ent_type_size * inner_dim * 2]
+        outputs = self.dense(last_hidden_state)
+        # output[0].shape=[batch_size, seq_len, self.inner_dim * 2]
+        # output[1].shape=[batch_size, seq_len, self.inner_dim * 2]
+        # ......
+        # output[-1].shape=[batch_size, seq_len, self.inner_dim * 2]
+        outputs = torch.split(outputs, self.inner_dim * 2, dim=-1)  # 列表
+        # outputs.shape=[batch_size, seq_len, ent_type_size, inner_dim * 2]
+        outputs = torch.stack(outputs, dim=-2)
+        # qw.shape=[batch_size, seq_len, ent_type_size, inner_dim]
+        # kw.shape=[batch_size, seq_len, ent_type_size, inner_dim]
+        qw, kw = outputs[..., :self.inner_dim], outputs[..., self.inner_dim:]
+
+        if self.RoPE:
+            # pos_emb.shape=[batch_size, seq_len, inner_dim]
+            pos_emb = self.sinusoidal_position_embedding(batch_size, seq_len, self.inner_dim)
+            # cos_pos.shape=[batch_size, seq_len, 1, inner_dim]
+            # sin_pos.shape=[batch_size, seq_len, 1, inner_dim]
+            cos_pos = pos_emb[..., None, 1::2].repeat_interleave(2, dim=-1)
+            sin_pos = pos_emb[..., None, ::2].repeat_interleave(2, dim=-1)
+            # qw2.shape=[batch_size, seq_len, inner_dim // 2, 2]
+            qw2 = torch.stack([-qw[..., 1::2], qw[..., ::2]], -1)
+            # qw2.shape=[batch_size, seq_len, inner_dim]
+            qw2 = qw2.reshape(qw.shape)
+            # qw.shape=[batch_size, seq_len, ent_type_size, inner_dim]
+            qw = qw * cos_pos + qw2 * sin_pos
+            kw2 = torch.stack([-kw[..., 1::2], kw[..., ::2]], -1)
+            kw2 = kw2.reshape(kw.shape)
+            kw = kw * cos_pos + kw2 * sin_pos
+        # logits.shape=[batch_size, ent_type_size, seq_len, seq_len]
+        logits = torch.einsum('bmhd,bnhd->bhmn', qw, kw)  # 计算内积
+        
+        # 排除padding
+        # attention.shape=[batch_size, seq_len]
+        # pad_mask.shape=[batch_size, seq_len, seq_len, seq_len]
+        pad_mask = attention_mask.unsqueeze(1).unsqueeze(1).expand(batch_size, self.ent_type_size, seq_len, seq_len)
+        logits = logits * pad_mask - (1 - pad_mask) * 1e12  # padding部分值为很小的负数
+
+        # 排除下三角
+        mask = torch.tril(torch.ones_like(logits), -1)
+        logits = logits - mask * 1e12
+
+        return logits / self.inner_dim ** 0.5
+
---
+++ I_Model/信息抽取/GlobalPointer/models/utils.py
@@ -0,0 +1,65 @@
+import torch
+import numpy as np
+
+
+def multilabel_categorical_crossentropy(y_pred, y_true):
+    """多标签分类的交叉熵.原理见https://kexue.fm/archives/7359"""
+    y_pred = (1 - 2 * y_true) * y_pred  # -1 -> pos classes, 1 -> neg classes
+    y_pred_neg = y_pred - y_true * 1e12  # mask the pred outputs of pos classes
+    y_pred_pos = y_pred - (1 - y_true) * 1e12  # mask the pred outputs of neg classes
+    zeros = torch.zeros_like(y_pred[..., :1])
+    y_pred_neg = torch.cat([y_pred_neg, zeros], dim=-1)
+    y_pred_pos = torch.cat([y_pred_pos, zeros], dim=-1)
+    neg_loss = torch.logsumexp(y_pred_neg, dim=-1)
+    pos_loss = torch.logsumexp(y_pred_pos, dim=-1)
+    return (neg_loss + pos_loss).mean()
+
+
+def loss_fun(y_true, y_pred):
+    """
+    y_true.shape=[batch_size, ent_type_size, seq_len, seq_len]
+    y_pred.shape=[batch_size, ent_type_size, seq_len, seq_len]
+    """
+    batch_size, ent_type_size = y_pred.shape[:2]
+    y_true = y_true.reshape(batch_size * ent_type_size, -1)
+    y_pred = y_pred.reshape(batch_size * ent_type_size, -1)
+    loss = multilabel_categorical_crossentropy(y_true, y_pred)
+    return loss
+
+
+class MetricsCalculator:
+    """查准率、查全率、F1 score"""
+    
+    def __init__(self):
+        self.TP = 0
+        self.TP_FP = 0
+        self.TP_FN = 0       
+    
+    def calc_confusion_matrix(self, y_pred, y_true):
+        self.TP, self.TP_FP, self.TP_FN = 1e-15, 1e-15, 1e-15
+        y_pred = y_pred.data.cpu().numpy()
+        y_true = y_true.data.cpu().numpy()
+        pred = []
+        true = []
+        for b, l, start, end in zip(*np.where(y_pred > 0.0)):  # 阈值(threshold)设置为0.0
+            pred.append((b, l, start, end))
+        for b, l, start, end in zip(*np.where(y_true > 0.0)):
+            true.append((b, l, start, end))
+
+        R = set(pred)
+        T = set(true)
+        self.TP += len(R & T)
+        self.TP_FP += len(R) 
+        self.TP_FN += len(T) 
+
+    @property
+    def precision(self):
+        return self.TP / self.TP_FP  # 查准率
+
+    @property
+    def recall(self):
+        return self.TP / self.TP_FN  # 查全率
+
+    @property
+    def f1(self):
+        return 2 * self.TP / (self.TP_FP + self.TP_FN)  # f1 score

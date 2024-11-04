d6bc0ffd328a78b7f16c8d5fddbb5177cfaddd55
dcdmm <dcdmmcomeon@gmail.com>
2024-11-04 13:56:37 +0800

    简单完善

--- F_Tool/other/深度学习可视化wandb模块/快速入门.py
+++ F_Tool/other/深度学习可视化wandb模块/快速入门.py
@@ -7,7 +7,7 @@ wandb.login()
 # start a new wandb run to track this script
 wandb.init(
     # set the wandb project where this run will be logged
-    project="my-awesome-project1ffa",
+    project="my-awesome-project",
 
     # track hyperparameters and run metadata
     config={

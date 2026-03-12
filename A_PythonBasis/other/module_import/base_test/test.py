9229f5b90558c6ba2678f6c58f221606d19494d5
dcdmm <dcdmmcomeon@gmail.com>
2026-03-12 16:22:28 +0800

    后面还需要完善

--- A_PythonBasis/other/module_import/base_test/test.py
+++ A_PythonBasis/other/module_import/base_test/test.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+
+print(os.getcwd())
 path = os.path.abspath("..")
 print("path:", path)
 sys.path.append(path)
@@ -8,18 +10,18 @@ print(os.path.abspath("../.."))
 sys.path.append(os.path.abspath(".." + os.sep + ".."))
 
 # test.py与print_hello.py位于不同目录结构下
-# test.py的目录结构为compare_test
-# print_hello.py的目录结构为compare
-# ===>解决1:从compare处开始导入(即目录结构与compare平级)
+# test.py的目录结构为base_test
+# print_hello.py的目录结构为base
+# ===>解决1:从base处开始导入(即目录结构与base平级)
 from base.print_hello import print_hello
 from base.print_hello1 import print_hello1
 print_hello("php")
 print_hello1('C++')
 
-# ===>解决2:从比compare目录结构更高的位置开始导入(即目录结构比compare更高)
+# ===>解决2:从比base目录结构更高的位置开始导入(即目录结构比base更高)
 from module_import.base.print_hello1 import print_hello1
 
 print('#' * 100)
 
 # 存在相对路径导入的代码不能直接执行
-from ..base.print_hello import print_hello
+# from ..base.print_hello import print_hello
